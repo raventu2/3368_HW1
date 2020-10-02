fd25e6fb8c1e2ca0ec9a3ee039ca0c41aa3817cd
ventu <venturarosie36@gmail.com>
2020-10-02 15:51:03 -0500

    Porche complete (hopefully)

--- homework1-raventu2-master/src/homework1/Mazda.java
+++ homework1-raventu2-master/src/homework1/Mazda.java
@@ -1,11 +1,17 @@
 package homework1;
 
-public class Mazda extends Vehicle
+public class Mazda extends Vehicle implements StreetCarFunctionalities
 {
     Mazda(double p, String m, int g)
     {
         super(p, m, g);
+        m = "Mazda5";
     }
 
+    public String superEconomic(String supEco)
+    {
+        return "Most economical car in my garage.";
+    }
 
+    //interface
 }
--- homework1-raventu2-master/src/homework1/Porsche.java
+++ homework1-raventu2-master/src/homework1/Porsche.java
@@ -1,5 +1,16 @@
 package homework1;
 
-public class Porsche {
+public class Porsche extends Vehicle implements RaceCarFunctionalities
+{
+    public Porsche(double p, String m, int g)
+    {
+        super(p, m, g);
+    }
 
+    public String superService(String supServ)
+    {
+        return "I need this every three months";
+    }
+
+    //interface
 }
